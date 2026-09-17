b966c4e2f49108e1c0738108612b4e20a44fc3db
Giovanny Yera <giovannyyera.mo@gmail.com>
2026-09-17 09:30:36 -0300

    feat: add dynamic article sitemap

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArticleSitemapController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/sitemap-articles.xml', ArticleSitemapController::class)
+    ->name('sitemap.articles');
 
 Route::get('/', function () {
     return view('welcome');
